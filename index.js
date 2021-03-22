d222002704f5f13af18601387db4d1ea4c0a56f4
Nima Poshtiban <poshtibannima@gmail.com>
2021-03-22 21:34:43 +0430

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'bookinventory';
+const adapterConfig = { mongoUri: 'mongodb://localhost/bookinventory' };
+
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp(), new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true })],
+};
